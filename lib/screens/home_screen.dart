9b262f3052cb05501ebed1a4e4d48f8cf41f8698
Hamid <haha.hamid.haha1380@gmail.com>
2022-08-20 14:32:34 +0430

    cRud

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_list/constants/constants.dart';
 import 'package:todo_list/models/todo_model.dart';
 import 'package:todo_list/screens/todo_screen.dart';
@@ -32,6 +33,34 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: kIconColor,
         child: const Icon(Icons.access_time),
       ),
+      body: FutureBuilder(
+        future: Hive.openBox('todoDB'),
+        builder: ((context, snapshot) {
+          return ToDoList();
+        }),
+      ),
     );
+  }
+
+  Widget ToDoList() {
+    Box todoBox = Hive.box('todoDB');
+    return ValueListenableBuilder(
+        valueListenable: todoBox.listenable(),
+        builder: ((context, Box box, child) {
+          if (box.values.isEmpty) {
+            return Text('No Task');
+          } else {
+            return ListView.builder(
+              itemCount: todoBox.length,
+              itemBuilder: (context, index) {
+                final ToDo todo = box.getAt(index);
+                return ListTile(
+                  title: Text(todo.title),
+                  subtitle: Text(todo.desc),
+                );
+              },
+            );
+          }
+        }));
   }
 }
--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:todo_list/models/todo_model.dart';
@@ -26,7 +27,11 @@ class ToDoScreen extends StatelessWidget {
             controller: desc,
           ),
           ElevatedButton(
-            onPressed: () => add(title.text, desc.text, false),
+            onPressed: () {
+              add(title.text, desc.text, false);
+              Navigator.pop(context);
+
+            },
             child: Text('save'),
           ),
         ],
@@ -37,7 +42,6 @@ class ToDoScreen extends StatelessWidget {
   void add(String title, String desc, bool isDone) async {
     var box = await Hive.openBox('todoDB');
     ToDo toDo = ToDo(desc, isDone, title);
-    int result = await box.add(toDo);
-    print(result);
+    await box.add(toDo);
   }
 }
